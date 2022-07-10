59d1d795ddf514d4a69ffdaf152818d70849fe0e
Benjamin Held <me@bheld.eu>
2022-07-10 16:50:34 +0200

    remember recent item information
    * store timestamp and wid of recent rss item
    * read recent item information to determine new rss items

--- lib/start.rb
+++ lib/start.rb
@@ -1,16 +1,20 @@
 require "rss"
 require "open-uri"
 require "net/smtp"
+require "pathname"
+
 require_relative "configuration"
 
 # What feed are we parsing?
 rss_feed = "https://wid.cert-bund.de/content/public/securityAdvisory/rss"
 
+config_path="../config/"
+
 def send_mail(item)
 
   wid = item.link.split("=")[1]
   timestamp = item.pubDate.localtime
-  config = Configuration.new("../config/config.json")
+  config = Configuration.new(Pathname.new(config_path).join("config.json").expand_path)
   
   message = "From: CERT RSS <#{config.config_hash["from"]}>\n"
   message.concat("To: #{config.config_hash["to"]}\n")
@@ -32,15 +36,38 @@ def send_mail(item)
 end
 
 URI.open(rss_feed) do |rss|
+
+  meta_path = Pathname.new(config_path).join("meta_info").expand_path
+  input_wid = nil
+  input_time = nil
+
+  # fetch the timestamp and wid from the file to check when to stop
+  if (meta_path.exist?)
+    input = File.readlines(meta_path)
+    input_wid = input[0].split(":")[1].strip
+    input_time = Time.parse(input[1].split(":")[1])
+  end
+
   feed = RSS::Parser.parse(rss)
   puts "Title: #{feed.channel.title}"
   feed.items.each { |item|
-    puts "Item: #{item.title}"
-    puts "Item description: #{item.description}"
-    puts "Item link: #{item.link}"
-    puts "Item date: #{item.pubDate}"
-    puts "Item category: #{item.category.content}"
-    puts "Item WID: #{item.link.split("=")[1]}"
-    send_mail(item)
+    item_wid = item.link.split("=")[1]
+    if (!item_wid.eql?(input_wid) && !item.pubDate.eql?(input_time))
+      puts "Item: #{item.title}"
+      puts "Item description: #{item.description}"
+      puts "Item link: #{item.link}"
+      puts "Item date: #{item.pubDate}"
+      puts "Item category: #{item.category.content}"
+      puts "Item WID: #{item_wid}"
+      send_mail(item)
+    else
+      break
+    end
   }
+
+  # write the youngest item in a file with its timestamp and wid to remember for the next run
+  output = File.open(meta_path, mode="w+")
+  first_item = feed.items.first
+  output.puts("Item WID: #{first_item.link.split("=")[1]}")
+  output.puts("Item date: #{first_item.pubDate.localtime}")
 end

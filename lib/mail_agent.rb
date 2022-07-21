c4dc7084688174acbcb8b38bf1a53ca78407a265
Benjamin Held <me@bheld.eu>
2022-07-21 16:26:07 +0200

    refactor mailing and advisory parsing (#3)

---
+++ lib/mail_agent.rb
@@ -0,0 +1,53 @@
+require "net/smtp"
+
+require_relative "configuration"
+require_relative "advisory_parser"
+
+module MailAgent
+
+  def self.send_mail(item, config_path)
+    wid = item.link.split("=")[1]
+    timestamp = item.pubDate.localtime
+    config = Configuration.new(Pathname.new(config_path).join("config.json").expand_path)
+    
+    message = "From: CERT RSS <#{config.config_hash["from"]}>\n"
+    message.concat("To: #{config.config_hash["to"]}\n")
+    message.concat("Subject: CERT Report (#{wid}) - #{item.title.split(":")[0]}\n\n")
+    message.concat("Our CERT RSS Feed received a new security advisory:\n\n")
+    message.concat("Title: #{item.title}\n")
+    message.concat("Description: #{item.description}\n")
+    message.concat("Link: #{item.link}\n")
+    message.concat("Date: #{timestamp}\n")
+    message.concat("#{retrieve_cves(wid)}\n")
+    message.concat("Severity: #{item.category.content}\n")
+    message.concat("WID: #{wid}\n\n")
+    message.concat("Best wishes,\n")
+    message.concat("Your CERT Bot.")
+
+    Net::SMTP.start(config.config_hash["address"], config.config_hash["port"], config.config_hash["helo"], 
+                    config.config_hash["user"], config.config_hash["password"], :plain) do |smtp|
+      smtp.send_message(message, config.config_hash["from"], config.config_hash["to"])
+    end
+    nil
+  end
+
+  private_class_method def self.retrieve_cves(wid)
+    cve_list = AdvisoryParser.retrieve_cves(wid)
+
+    cves = "CVEs: "
+    counter = 0
+    cve_list.each { |cve_id|
+      cves.concat(cve_id["cveId"]).concat(" ")
+      counter += 1
+
+      # write only 5 cve per line then make linebreak
+      if (counter == 5)
+        cves.concat("\n")
+        6.times { cves.concat(" ") }
+        counter = 0
+      end
+    }
+    cves
+  end 
+
+end
--- lib/start.rb
+++ lib/start.rb
@@ -1,72 +1,13 @@
 require "rss"
 require "open-uri"
-require "net/smtp"
 require "pathname"
-require "net/http"
-require "json"
 
-require_relative "configuration"
+require_relative "mail_agent"
 
 # What feed are we parsing?
 rss_feed = "https://wid.cert-bund.de/content/public/securityAdvisory/rss"
 
-config_path="../config/"
-
-def filter_flat_map(list, type) 
-  list["children"].flat_map {|child| 
-    yield child if child["type"] == type 
-  }.compact
-end
-
-def retrieve_cves(wid)
-  uuid_url = "https://wid.cert-bund.de/content/public/securityAdvisory/kurzinfo-uuid-by-name/#{wid}"
-  wid_request = Net::HTTP.get(URI(uuid_url))
-  cert_url = "https://wid.cert-bund.de/content/public/content/#{JSON.parse(wid_request)}"
-  cert_request = Net::HTTP.get(URI(cert_url))
-  cert_json = JSON.parse(cert_request)
-  cve_list = filter_flat_map(cert_json, "cveIdListe") {|cve_id_list| filter_flat_map(cve_id_list, "cveId") {|note| note["properties"] } }
-
-  cves = "CVEs: "
-  counter = 0
-  cve_list.each { |cve_id|
-    cves.concat(cve_id["cveId"]).concat(" ")
-    counter += 1
-
-    # write only 5 cve per line then make linebreak
-    if (counter == 5)
-      cves.concat("\n")
-      6.times { cves.concat(" ") }
-      counter = 0
-    end
-  }
-  cves
-end  
-
-def send_mail(item, config_path)
-
-  wid = item.link.split("=")[1]
-  timestamp = item.pubDate.localtime
-  config = Configuration.new(Pathname.new(config_path).join("config.json").expand_path)
-  
-  message = "From: CERT RSS <#{config.config_hash["from"]}>\n"
-  message.concat("To: #{config.config_hash["to"]}\n")
-  message.concat("Subject: CERT Report (#{wid}) - #{item.title.split(":")[0]}\n\n")
-  message.concat("Our CERT RSS Feed received a new security advisory:\n\n")
-  message.concat("Title: #{item.title}\n")
-  message.concat("Description: #{item.description}\n")
-  message.concat("Link: #{item.link}\n")
-  message.concat("Date: #{timestamp}\n")
-  message.concat("#{retrieve_cves(wid)}\n")
-  message.concat("Severity: #{item.category.content}\n")
-  message.concat("WID: #{wid}\n\n")
-  message.concat("Best wishes,\n")
-  message.concat("Your CERT Bot.")
-  
-  Net::SMTP.start(config.config_hash["address"], config.config_hash["port"], config.config_hash["helo"], 
-                  config.config_hash["user"], config.config_hash["password"], :plain) do |smtp|
-    smtp.send_message(message, config.config_hash["from"], config.config_hash["to"])
-  end
-end
+config_path ="../config/"
 
 URI.open(rss_feed) do |rss|
 
@@ -93,7 +34,7 @@ URI.open(rss_feed) do |rss|
         puts "Item date: #{item.pubDate}"
         puts "Item category: #{item.category.content}"
         puts "Item WID: #{item_wid}"
-        send_mail(item, config_path)
+        MailAgent.send_mail(item, config_path)
       end
     else
       break

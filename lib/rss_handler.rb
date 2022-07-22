4329577af855be292420e3e05a1a6113cbff4233
Benjamin Held <me@bheld.eu>
2022-07-22 22:34:21 +0200

    refactor rss_handler

---
+++ lib/rss_handler.rb
@@ -0,0 +1,70 @@
+require "rss"
+require "open-uri"
+require "pathname"
+
+class RssHandler
+
+  def initialize(rss_feed, config_path)
+    read_feed(rss_feed, config_path)    
+  end
+
+  private
+
+  def read_feed(rss_feed, config_path)
+    URI.open(rss_feed) do |rss|
+      meta_path = Pathname.new(config_path).join("meta_info").expand_path
+      meta_data = read_meta_data(meta_path)
+
+      feed = RSS::Parser.parse(rss)
+      puts "Title: #{feed.channel.title}"
+      feed.items.each { |item|
+        item_wid = item.link.split("=")[1]
+        if (!item_wid.eql?(meta_data.wid) && !item.pubDate.eql?(meta_data.timestamp))
+          if (item.category.content.eql?("hoch") || item.category.content.eql?("kritisch"))
+            puts "Item: #{item.title}"
+            puts "Item description: #{item.description}"
+            puts "Item link: #{item.link}"
+            puts "Item date: #{item.pubDate}"
+            puts "Item category: #{item.category.content}"
+            puts "Item WID: #{item_wid}"
+            MailAgent.send_mail(item, config_path)
+          end
+        else
+          break
+        end
+      }
+
+      write_meta_data(meta_path, feed.items.first)
+    end
+  end
+
+  def read_meta_data(meta_path)
+    if (meta_path.exist?)
+      input = File.readlines(meta_path)
+      return MetaData.new(input[0].split(":")[1].strip,
+                   Time.parse(input[1].split(":")[1]))
+    end
+    MetaData.new()
+  end
+  
+  def write_meta_data(meta_path, item)
+    output = File.open(meta_path, mode="w+")
+    output.puts("Item WID: #{item.link.split("=")[1]}")
+    output.puts("Item date: #{item.pubDate.localtime}")
+    output.close
+    nil
+  end
+
+end
+
+class MetaData
+
+  attr_accessor :wid
+  attr_accessor :timestamp
+
+  def initialize(wid = nil, timestamp = nil)
+    @wid = wid
+    @timestamp = timestamp
+  end
+
+end
--- lib/start.rb
+++ lib/start.rb
@@ -1,49 +1,9 @@
-require "rss"
-require "open-uri"
-require "pathname"
-
 require_relative "mail_agent"
+require_relative "rss_handler"
 
 # What feed are we parsing?
 rss_feed = "https://wid.cert-bund.de/content/public/securityAdvisory/rss"
 
 config_path ="../config/"
 
-URI.open(rss_feed) do |rss|
-
-  meta_path = Pathname.new(config_path).join("meta_info").expand_path
-  input_wid = nil
-  input_time = nil
-
-  # fetch the timestamp and wid from the file to check when to stop
-  if (meta_path.exist?)
-    input = File.readlines(meta_path)
-    input_wid = input[0].split(":")[1].strip
-    input_time = Time.parse(input[1].split(":")[1])
-  end
-
-  feed = RSS::Parser.parse(rss)
-  puts "Title: #{feed.channel.title}"
-  feed.items.each { |item|
-    item_wid = item.link.split("=")[1]
-    if (!item_wid.eql?(input_wid) && !item.pubDate.eql?(input_time))
-      if (item.category.content.eql?("hoch") || item.category.content.eql?("kritisch"))
-        puts "Item: #{item.title}"
-        puts "Item description: #{item.description}"
-        puts "Item link: #{item.link}"
-        puts "Item date: #{item.pubDate}"
-        puts "Item category: #{item.category.content}"
-        puts "Item WID: #{item_wid}"
-        MailAgent.send_mail(item, config_path)
-      end
-    else
-      break
-    end
-  }
-
-  # write the youngest item in a file with its timestamp and wid to remember for the next run
-  output = File.open(meta_path, mode="w+")
-  first_item = feed.items.first
-  output.puts("Item WID: #{first_item.link.split("=")[1]}")
-  output.puts("Item date: #{first_item.pubDate.localtime}")
-end
+RssHandler.new(rss_feed, config_path)

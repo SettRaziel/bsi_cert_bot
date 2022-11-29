652a94dcdcddf29fd8c04c9d1dedd219522a21e3
Benjamin Held <me@bheld.eu>
2022-11-29 07:47:16 +0100

    add configuration tests for tls parameter

--- spec/cert_bot/configuration_spec.rb
+++ spec/cert_bot/configuration_spec.rb
@@ -30,4 +30,56 @@ describe CertBot::Configuration do
     end
   end
 
+  describe ".new" do
+    context "given a file with config data" do
+      it "read the json data and set the attributes without an error" do
+        json_hash = Hash.new()
+        json_hash["address"]  = "example.com"
+        json_hash["port"]     = "587"
+        json_hash["helo"]     = "me@example.com"
+        json_hash["user"]     = "me"
+        json_hash["password"] = "password"
+        json_hash["from"]     = "me@example.com"
+        json_hash["to"]       = "you@example.com"
+        json_hash["authtype"] = "login"
+
+        JSON.pretty_generate(json_hash)
+        file = File.open("configuration.json", "w")
+        file.write(JSON.pretty_generate(json_hash))
+        file.close
+
+        configuration = CertBot::Configuration.new("configuration.json")
+        expect(configuration.config_hash["authtype"]).to eq(:login)
+
+        File.delete("configuration.json")
+      end
+    end
+  end
+
+  describe ".new" do
+    context "given a file with config data" do
+      it "read the json data and set the attributes without an error" do
+        json_hash = Hash.new()
+        json_hash["address"]  = "example.com"
+        json_hash["port"]     = "587"
+        json_hash["helo"]     = "me@example.com"
+        json_hash["user"]     = "me"
+        json_hash["password"] = "password"
+        json_hash["from"]     = "me@example.com"
+        json_hash["to"]       = "you@example.com"
+        json_hash["tls_verify"] = "false"
+
+        JSON.pretty_generate(json_hash)
+        file = File.open("configuration.json", "w")
+        file.write(JSON.pretty_generate(json_hash))
+        file.close
+
+        configuration = CertBot::Configuration.new("configuration.json")
+        expect(configuration.config_hash["tls_verify"]).to eq(false)
+
+        File.delete("configuration.json")
+      end
+    end
+  end
+
 end
